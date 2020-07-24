d4015dc7082bbb4545cd05aafc4c641379e27dc8
Arthur Costa da Silva <arthur.silva@pampa.compasso>
2020-07-24 18:07:34 -0300

    feat: add components into the home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,45 @@
 import React from "react";
-import { Text } from "react-native";
+import { MaterialCommunityIcons, AntDesign } from "@expo/vector-icons";
+
+import Suggestions from "../../shared/components/Suggestions";
+import Activities from "../../shared/components/Activities";
+import Tips from "../../shared/components/Tips";
+import Banner from "../../shared/components/Banner";
+import {
+  Wrapper,
+  Container,
+  Header,
+  BalanceContainer,
+  BalanceTitle,
+  Balance,
+} from "./styles";
 
 const Home = () => {
-  return <Text>Home</Text>;
+  return (
+    <Wrapper>
+      <Container>
+        <Header>
+          <MaterialCommunityIcons
+            name="qrcode-scan"
+            size={30}
+            color="#10c86e"
+          />
+
+          <BalanceContainer>
+            <BalanceTitle>My balance</BalanceTitle>
+            <Balance>$ 0.00</Balance>
+          </BalanceContainer>
+
+          <AntDesign name="gift" size={30} color="#10c86e" />
+        </Header>
+
+        <Suggestions />
+        <Activities />
+        <Tips />
+        <Banner />
+      </Container>
+    </Wrapper>
+  );
 };
 
 export default Home;
